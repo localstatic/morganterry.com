0b27e133d771996421fc0cfe4a23dfe4aa853105
Morgan Terry <localstatic@localstatic.com>
2013-05-15 22:44:27 -0600

    Add basic skeleton for "sample work" slideshow

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+$(function() {
+	$('#sample-work-slideshow')
+		.before('<a href="#" id="sample-work-slideshow-prev" class="slideshow-prev">Prev</a> <a href="#" id="sample-work-slideshow-next" class="slideshow-next">Next</a>')
+		.after('<div id="sample-work-slideshow-nav">')
+		.cycle({
+			fx: "scrollHorz",
+			pager: "#sample-work-slideshow-nav",
+			next: "#sample-work-slideshow-next",
+			prev: "#sample-work-slideshow-prev",
+			delay: 5
+		});
+});
